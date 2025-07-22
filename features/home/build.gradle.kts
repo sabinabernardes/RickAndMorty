34fb99a05b80ce298417030f447ebbda4573e063
Sabina Bernardes <66752747+sabinabernardes@users.noreply.github.com>
2025-07-22 09:53:03 -0300

    feat: adjusting gradle

--- features/home/build.gradle.kts
+++ features/home/build.gradle.kts
@@ -27,11 +27,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     buildFeatures {
         compose = true
+    }
+    kotlinOptions {
+        jvmTarget = "11"
     }
 }
 
